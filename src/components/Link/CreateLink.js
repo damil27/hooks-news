59d4adc207f0a3a4371804b013df1814a87f40fb
damil27 <dhamilare27@gmail.com>
2022-10-15 20:51:04 +0100

    CreateLink page

--- src/components/Link/CreateLink.js
+++ src/components/Link/CreateLink.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function CreateLink(props) {
-  return <div>CreateLink</div>;
+  return (
+    <form className="flex flex-columm mt3">
+      <input
+        name="description"
+        placeholder=" A description for your link"
+        autoComplete="off"
+      />
+      <input name="url" placeholder="the Url for the link" autoComplete="off" />
+      <button className="button" type="submit">
+        submit
+      </button>
+    </form>
+  );
 }
 
 export default CreateLink;
